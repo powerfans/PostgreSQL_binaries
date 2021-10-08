88eacf7ce750596c0af8963d2c5e0575b07869e7
lsq <nikelsq@sina.com>
2021-10-08 11:59:13 +0800

    origin commit on 2021-10-8

--- scripts/src/13.4/GET_SOURCE.links.sh
+++ scripts/src/13.4/GET_SOURCE.links.sh
@@ -6,7 +6,7 @@
 wget -c https://ftp.postgresql.org/pub/source/v13.4/postgresql-13.4.tar.gz --no-check-certificate
 
 ##2. SRC RPM
-wget -c https://yum.postgresql.org/srpms/13/redhat/rhel-7-ppc64le/postgresql13-13.4-1PGDG.rhel7.src.rpm --no-check-certificate --no-check-certificate
+wget -c https://yum.postgresql.org/srpms/13/redhat/rhel-7-ppc64le/postgresql13-13.4-1PGDG.rhel7.src.rpm --no-check-certificate 
 
 
 
