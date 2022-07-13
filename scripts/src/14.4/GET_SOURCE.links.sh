4a86844110a682601be6e3411306cc5b44429943
lsq <nikelsq@sina.com>
2022-07-13 13:42:28 +0800

    origin commit on 2022-07-12

---
+++ scripts/src/14.4/GET_SOURCE.links.sh
@@ -0,0 +1,12 @@
+### https://www.postgresql.org/ftp/source/
+### https://yum.postgresql.org/rpmchart/
+### https://https://yum.postgresql.org/srpm-packages/
+
+##1.TAR source
+wget -c https://ftp.postgresql.org/pub/source/v14.4/postgresql-14.4.tar.gz --no-check-certificate
+
+##2. SRC RPM
+wget -c https://download.postgresql.org/pub/repos/yum/srpms/14/redhat/rhel-7.9-ppc64le/postgresql14-14.4-1PGDG.rhel7.src.rpm --no-check-certificate
+wget -c https://download.postgresql.org/pub/repos/yum/srpms/14/redhat/rhel-8.9-ppc64le/postgresql14-14.4-2PGDG.rhel8.src.rpm --no-check-certificate
+
+
