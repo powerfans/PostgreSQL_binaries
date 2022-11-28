ac6daea93a7e194232d29451cb0d935f13b0e084
lsq <nikelsq@sina.com>
2022-11-28 15:38:38 +0800

    commit on 2022-11-28

--- scripts/src/15.1/GET_SOURCE.links.sh
+++ scripts/src/15.1/GET_SOURCE.links.sh
@@ -3,9 +3,10 @@
 ### https://https://yum.postgresql.org/srpm-packages/
 
 ##1.TAR source
-wget -c https://ftp.postgresql.org/pub/source/v15beta2/postgresql-15beta2.tar.gz --no-check-certificate
+wget -c https://ftp.postgresql.org/pub/source/v15.1/postgresql-15.1.tar.gz  --no-check-certificate
 
 ##2. SRC RPM
-wget -c https://download.postgresql.org/pub/repos/yum/srpms/15/redhat/rhel-7.9-ppc64le/postgresql15-15.1-1PGDG.rhel7.src.rpm --no-check-certificate
+wget -c https://download.postgresql.org/pub/repos/yum/srpms/15/redhat/rhel-7.6-ppc64le/postgresql15-15.1-1PGDG.rhel7.src.rpm --no-check-certificate
+wget -c https://download.postgresql.org/pub/repos/yum/srpms/15/redhat/rhel-8.9-ppc64le/postgresql15-15.1-1PGDG.rhel8.src.rpm --no-check-certificate
 
 
